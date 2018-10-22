ddcffc99d8ddb5f2ac409d91cd20b2bfe183cd4d
LiTongWork <2272701464@qq.com>
2018-10-22 17:35:22 +0800

    wash

--- pages/wash/wash.js
+++ pages/wash/wash.js
@@ -23,65 +23,29 @@ Page({
   /*** 页面的初始数据*/
   data: {
     arr1:[],
-    arr1Index:0,
-    prompt1:'选择衣服种类',
-    price:'',
-    count: 0
+    arr2:[],
+    count:'1'
+    
   },
 
   /*** 生命周期函数--监听页面加载*/
   onLoad: function (options) {
-    var reqTask = wx.request({
-      url: `${URL}order/GetClothesType`,
-      header: header,
-      method: 'POST',
-      dataType: 'json',
-      responseType: 'text',
-      success: (res)=>{
-        this.setData({
-          arr1: res.data.Data
-        })
-      },
-      fail: ()=>{},
-      complete: ()=>{}
-    });
-  },
-  picker1Change:function(e){
-    this.setData({
-      arr1Index:e.detail.value
-    })
-   
-    // this.setData({
-    //   prompt1: this.data.arr1[this.data.arr1Index].dd1
-    // })
-
-  },
-  picker2Change: function (e) {
-    this.setData({
-      arr2Index: e.detail.value
-    })
-    this.setData({
-      prompt2: this.data.arr2[this.data.arr2Index].dd2
-    })
-
-  },
-  minus: function() {
-    if(this.data.count>0) {
-      let count = this.data.count;
-      this.setData({
-        count: --count
-      })
-    }
-  },
-  add: function() {
-    let count = this.data.count;
-    count++;
-    const price = this.data.arr1[this.data.arr1Index].Price
-    this.setData({
-      count: count,
-      price: price*count
-    });
-
+   wx.request({
+     url: 'https://xwxapi.itknow.cn/api/order/GetClothesType',
+     header:header,
+     method:'post',
+     data:DATA,
+     dataType:'json',
+     responseType: 'text',
+     success: (res) => {
+       this.setData({
+         arr1: res.data.Data.ParentList,
+         arr2: res.data.Data.ClothesList
+       })
+       console.log(this.data.arr1)
+       console.log(this.data.arr2)
+     }
+   })
   },
   pay: function() {
     wx.request({
